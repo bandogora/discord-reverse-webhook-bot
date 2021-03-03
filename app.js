714dca33f2e3184f6451db3cfcc956e8d1a9cf2d
Justin Forgue <juforgue@gmail.com>
2021-03-02 23:03:42 -0500

    fix https request

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
-import { Client } from 'discord.js';
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const https = require('https')
-const client = new Client();
 
 client.once('ready', () => {
   console.log('Ready');
@@ -12,41 +11,36 @@ client.login(process.env.DISCORD_TOKEN);
 
 // Log all messages
 client.on('message', message => {
-	console.log(message.content);
-});
 
-// Parse message contents for commands
-switch (message.content) {
-	case '!start':
-		post_request('start');
-		message.channel.send('Starting server...');
-		break;
-	case '!stop':
-		post_request('stop');
-		message.channel.send('Starting server...');
-		break;
-	case '!restart':
-		post_request('restart');
-		message.channel.send('Restarting server...');
-		break;
-	case 'OMG':
-		message.channel.send('Yes?');
-		break;
-	case 'test':
-		message.channel.send("Don't test me.");
-		break;
-}
+  // Parse message contents for commands
+  switch (message.content) {
+    case '!start':
+      post_request('start');
+      message.channel.send('Starting server...');
+      break;
+    case '!stop':
+      post_request('stop');
+      message.channel.send('Starting server...');
+      break;
+    case '!restart':
+      post_request('restart');
+      message.channel.send('Restarting server...');
+      break;
+    case 'OMG':
+      message.channel.send('Yes?');
+      break;
+    case 'test':
+      message.channel.send("Don't test me.");
+      break;
+  }
+});
 
 function post_request(action) {
 	var data = JSON.stringify({
 		action: action
 	})
 
-	// https://9bb0105d-b61a-470c-ba2e-5555.webhook.wus.azure-automation.net/webhooks?token=oYW1RPIg3tqOtWR%2brJpU5c8wNhKKaXTV1SHUdBVV7P0%ef
   const options = {
-    hostname: process.env.WEBHOOK_SERVER,
-    path: '/webhooks',
-		token: process.env.WEBHOOK_TOKEN,
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -54,7 +48,7 @@ function post_request(action) {
     }
   }
 
-  const req = https.request(options, res => {
+  const req = https.request(process.env.WEBHOOK_URL, options, res => {
     console.log(`statusCode: ${res.statusCode}`)
 
     res.on('data', d => {
